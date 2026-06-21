7aaee16c1e1080ace3da963a4211d69e77859c18
Robera Mekonnen <Robera4553@gmail.com>
2026-06-21 15:02:01 +0300

    Force unregister old service workers to rescue clients from broken PWA cache

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Force unregister any lingering Service Workers from the previous PWA setup
+// This ensures returning visitors don't get stuck on "Initializing Subsystems..."
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (let registration of registrations) {
+            registration.unregister();
+            console.log('Unregistered old service worker.');
+        }
+    }).catch(err => console.error('Service worker unregistration failed:', err));
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
